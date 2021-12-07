6c806c11c730fd7eec088ea4420a39859bcba926
Gouja Ameni <gouja.ameni@gmail.com>
2021-12-07 12:07:05 +0100

    add all file

---
+++ src/Services.ts
@@ -0,0 +1,7 @@
+export const Services=[
+    {'name':'Web Development','price':300,'selected':true},
+    {'name':'Design','price':400,'selected':false},
+    {'name':'Integration','price':250,'selected':false},
+    {'name':'Formation','price':220,'selected':false},
+ 
+]
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Services } from 'src/Services';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exercice2';
+  services=Services;
+  nbreEemeSelected=1;
+  //selected=false;
+  unselected=true;
+  somme=300;
+  select(service){
+   if(service.selected==true)
+   { service.selected=false;
+    this.somme=this.somme-service.price;
+    this.nbreEemeSelected--;
+
+   }else{
+
+      service.selected=true;
+      this.nbreEemeSelected++;
+    this.somme+=service.price;
+   }
+
+    console.log(this.somme);}
+/*if(service.selected==false)
+{
+  this.selected=true;
+  this.somme=this.somme+price;
+}
+else{
+  this.selected=false;
+  this.somme=this.somme-price;
+}
+console.log(this.somme);
+    
+  }*/
 }
